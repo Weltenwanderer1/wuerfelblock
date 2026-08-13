a0d19ab600df3c64232c77d711559dcba3828c2c
Günther Schuch <Weltenwanderer1@users.noreply.github.com>
2026-08-13 09:17:41 +0200

    Keep screen awake while app is open

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:wakelock_plus/wakelock_plus.dart';
 
 import 'app.dart';
 import 'services/game_repository.dart';
@@ -7,6 +8,7 @@ import 'services/locale_controller.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await WakelockPlus.enable();
   final preferences = await SharedPreferences.getInstance();
   runApp(
     WuerfelblockApp(
